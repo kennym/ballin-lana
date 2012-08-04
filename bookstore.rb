4667b95748a935add0b535bcc46af59c5fc8ba99
Kenny Meyer <knny.myer@gmail.com>
2012-08-03 22:52:53 -0400

    Getting course materials

--- bookstore.rb
+++ bookstore.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'capybara'
 require 'capybara/dsl'
 require 'capybara-webkit'
+require 'debugger'
 
 Capybara.run_server = false
 Capybara.current_driver = :webkit
@@ -19,9 +20,18 @@ module Bookstore
 
     def initialize
       visit_front_page()
-      get_states()
+      select_state()
       select_institution()
       visit_textbooks_page()
+
+      select_program
+      select_term
+      select_department
+      select_course
+      select_section
+      submit_textbooks
+
+      parse_course_materials
     end
 
     def visit_front_page
@@ -29,13 +39,12 @@ module Bookstore
       visit("/")
     end
 
-    def get_states
-      puts "Getting states"
+    def select_state
+      puts "Selecting state"
       states = find(:xpath, "//*[@id='stateUSAIdSelect']").all("option")
       states.shift # Push off the first element with text 'Select your State'
 
       select states[0].text, :from => 'Select Your State'
-
     end
 
     def select_institution
@@ -60,9 +69,54 @@ module Bookstore
 
       find(:xpath, "//*[@id='efContentHome']/div[2]/a").click
 
-      wait_until { current_path.starts_with "/CategoryDisplay" }
+      wait_until { current_path.start_with? "/CategoryDisplay" }
+    end
 
-      puts page.html
+    def select_program
+      select "All", :from => "programId"
+    end
+
+    def select_term
+      select "Fall 2012", :from => "termId"
+    end
+
+    def select_department
+      wait_until { find(:xpath, "//*[@id='departmentIdSelect']").all("option").count > 1}
+
+      departments = find(:xpath, "//*[@id='departmentIdSelect']").all("option")
+      departments.shift
+
+      select departments.first.text, :from => "departmentIdSelect"
+    end
+
+    def select_course
+      wait_until { find(:xpath, "//*[@id='courseIdSelect']").all("option").count > 1}
+
+      courses = find(:xpath, "//*[@id='courseIdSelect']").all("option")
+      courses.shift
+
+      select courses.first.text, :from => "courseIdSelect"
+    end
+
+    def select_section
+      wait_until { find(:xpath, "//*[@id='sectionIdSelect']").all("option").count > 1}
+
+      sections = find(:xpath, "//*[@id='courseIdSelect']").all("option")
+      sections.shift
+
+      select sections.first.text, :from => "sectionIdSelect"
+    end
+
+    def submit_textbooks
+      wait_until { find_button "Submit" }
+
+      click_button_submit
+    end
+
+    def parse_course_materials
+      wait_until { current_path.start_with? "/webapp/wcs/stores/servlet/CourseMaterialsResultsView" }
+
+      puts "Wohoo"
     end
   end
 end
